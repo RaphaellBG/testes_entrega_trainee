0f7a5ab421c4c96a38636a65434b4fabfc856ea1
Tomás de Miranda <tcmp@outlook.com.br>
2021-02-27 11:01:43 -0300

    client rodando em loop

--- cliente.py
+++ cliente.py
@@ -1,12 +1,21 @@
 import paho.mqtt.client as paho
 
+client = paho.Client(client_id = "Trainees")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("czarspace_telemetria")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
 client = paho.Client()
+client.on_connect = on_connect
+client.on_message = on_message
 
-if client.connect("test.mosquitto.org", 1883, 60) != 0 :
-    print("Could not connect")
-    sys.exit(-1)
+client.connect("test.mosquitto.org", 1883, 60)
 
 
-#client.publish("statusczarspace", "Hello")
+client.loop_forever()
 
-client.disconnect()
