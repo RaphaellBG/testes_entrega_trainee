171d67bec56ca8771cebfae876646dc1095bca0a
Raphael B. Guilherme <raphaelbg6@gmail.com>
2021-02-27 10:43:53 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+#import json as js
+#import time
+
+print("teste")
+
+#dados = {
+#    "flag1": "00",
+#    "hora": ts = time.time(),
+#    "subsistema": "ADCS",
+#    "modo": 1,
+#    "telemetrias": [
+#        {"adcs": [23.4, 33.9, 80.0]}
+#        {"eps": [3.3 89]}
+#    ],
+#    "flag2": "00"
+#}
+
+#print(js.dumps(dados))
